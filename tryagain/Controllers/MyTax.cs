ec27b162c5a1cb331aceb7b64509dc9bc90887ba
Divine <dchukwudire@gmail.com>
2025-10-21 07:13:36 +0100

    first commit

---
+++ tryagain/Controllers/MyTax.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tryagain.Models;
+
+namespace tryagain.Controllers
+{
+    public class MyTax : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Tax());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Tax s)
+        {
+            if (s.TaxId == "NG" && s.Sal >= 500000)
+            {
+                s.Ta = s.Sal * 19 / 100;
+            }
+            else if(s.TaxId == "NG" && s.Sal >= 1600000)
+            {
+                s.Ta = s.Sal * 21 / 100;
+            }
+            else
+            {
+                s.Ta = s.Sal * 1 / 100;
+            }
+            return View();
+        }
+    }
+}
---
+++ tryagain/Models/Tax.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace tryagain.Models
+{
+    public class Tax
+    {
+        public string? TaxId { get; set; }
+
+        public float Sal { get; set; }
+
+        public float Ta { get; set; }
+    }
+}
